824d06e4bbd25d27f966748608bc6bc6ecfdf972
michael1735 <25171080@qq.com>
2023-05-27 12:39:04 +0800

    finished stage 3: fog of war

--- src/Player.java
+++ src/Player.java
@@ -18,13 +18,14 @@ public class Player {
          */
         while (!valid){
             if (coors[0] > 10 || coors[0] < 1 || coors[1] > 10 || coors[1] < 1) {
-                System.out.printf("%nError! You entered the wrong coordinates! Try again:%n%n> ");
+                System.out.printf("%nError! You entered wrong coordinates! Try again:%n%n> ");
                 reInput(coors);
                 continue;
             }
             if (board.emptyBoard[coors[0]][coors[1]].equals("O")) {
                 // set the coordinates that the ship takes to "X"
                 board.emptyBoard[coors[0]][coors[1]] = "X";
+                board.tempBoard[coors[0]][coors[1]] = "X";
                /* for (int i = 0; i < shipCoors.length; i++) {
                     for (int j = 0; j < shipCoors[i].length; j++) {
                         if (shipCoors[i][j][0] == coors[0] && shipCoors[i][j][1] == coors[1]) {
@@ -40,14 +41,17 @@ public class Player {
                     };
                     board.emptyBoard[coors[0]][coors[1]] = "X";
                 } // set all the coordinates accompanied by that ship to "X"*/
+                board.outEmptyBoard();
+                System.out.printf("%nYou hit a ship!%n%n");
                 board.outBoard();
-                System.out.printf("%nYou hit a ship!");
                 valid = true;
             } else {
                 board.emptyBoard[coors[0]][coors[1]] = "M";
+                board.tempBoard[coors[0]][coors[1]] = "M";
                 valid = true;
+                board.outEmptyBoard();
+                System.out.printf("%nYou missed!%n%n");
                 board.outBoard();
-                System.out.printf("%nYou missed!");
             }
         }
     }
